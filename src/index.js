52196d9ae21d28f4f36cd5d23ad729f23a0f4b75
Renstrio24p <Renstrio24p@users.noreply.github.com>
2024-01-07 14:25:47 +0000

    deploy: 9314875c810d8cd4714db15762111fa4ea8cefcd

---
+++ src/index.js
@@ -0,0 +1,232 @@
+"use strict";
+(() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __esm = (fn, res) => function __init() {
+    return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
+  };
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+
+  // <define:process.env>
+  var define_process_env_default;
+  var init_define_process_env = __esm({
+    "<define:process.env>"() {
+      define_process_env_default = { GITHUB_STATE: "/home/runner/work/_temp/_runner_file_commands/save_state_5ab42410-3d08-48c2-a5e1-b433bc7d1378", STATS_TRP: "true", DEPLOYMENT_BASEPATH: "/opt/runner", DOTNET_NOLOGO: "1", USER: "runner", npm_config_user_agent: "npm/8.19.4 node/v16.20.2 linux x64 workspaces/false ci/github-actions", CI: "true", RUNNER_ENVIRONMENT: "github-hosted", GITHUB_ENV: "/home/runner/work/_temp/_runner_file_commands/set_env_5ab42410-3d08-48c2-a5e1-b433bc7d1378", PIPX_HOME: "/opt/pipx", npm_node_execpath: "/opt/hostedtoolcache/node/16.20.2/x64/bin/node", JAVA_HOME_8_X64: "/usr/lib/jvm/temurin-8-jdk-amd64", SHLVL: "1", npm_config_noproxy: "", HOME: "/home/runner", RUNNER_TEMP: "/home/runner/work/_temp", GITHUB_EVENT_PATH: "/home/runner/work/_temp/_github_workflow/event.json", npm_package_json: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT/package.json", JAVA_HOME_11_X64: "/usr/lib/jvm/temurin-11-jdk-amd64", PIPX_BIN_DIR: "/opt/pipx_bin", GITHUB_REPOSITORY_OWNER: "Renstrio24p", GRADLE_HOME: "/usr/share/gradle-8.5", ANDROID_NDK_LATEST_HOME: "/usr/local/lib/android/sdk/ndk/26.1.10909125", JAVA_HOME_21_X64: "/usr/lib/jvm/temurin-21-jdk-amd64", STATS_RDCL: "true", GITHUB_RETENTION_DAYS: "90", GITHUB_REPOSITORY_OWNER_ID: "123795328", POWERSHELL_DISTRIBUTION_CHANNEL: "GitHub-Actions-ubuntu22", AZURE_EXTENSION_DIR: "/opt/az/azcliextensions", GITHUB_HEAD_REF: "", npm_config_userconfig: "/home/runner/.npmrc", npm_config_local_prefix: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT", SYSTEMD_EXEC_PID: "591", GITHUB_GRAPHQL_URL: "https://api.github.com/graphql", COLOR: "0", GOROOT_1_20_X64: "/opt/hostedtoolcache/go/1.20.12/x64", NVM_DIR: "/home/runner/.nvm", npm_config_metrics_registry: "https://registry.npmjs.org/", DOTNET_SKIP_FIRST_TIME_EXPERIENCE: "1", GOROOT_1_21_X64: "/opt/hostedtoolcache/go/1.21.5/x64", JAVA_HOME_17_X64: "/usr/lib/jvm/temurin-17-jdk-amd64", ImageVersion: "20231217.2.0", RUNNER_OS: "Linux", GITHUB_API_URL: "https://api.github.com", SWIFT_PATH: "/usr/share/swift/usr/bin", RUNNER_USER: "runner", STATS_V3PS: "true", CHROMEWEBDRIVER: "/usr/local/share/chromedriver-linux64", JOURNAL_STREAM: "8:19012", GITHUB_WORKFLOW: "Deploy", _: "/opt/hostedtoolcache/node/16.20.2/x64/bin/npm", npm_config_prefix: "/opt/hostedtoolcache/node/16.20.2/x64", npm_config_cache: "/home/runner/.npm", ACTIONS_RUNNER_ACTION_ARCHIVE_CACHE: "/opt/actionarchivecache", GITHUB_RUN_ID: "7438822804", GITHUB_REF_TYPE: "branch", BOOTSTRAP_HASKELL_NONINTERACTIVE: "1", GITHUB_WORKFLOW_SHA: "9314875c810d8cd4714db15762111fa4ea8cefcd", GITHUB_BASE_REF: "", ImageOS: "ubuntu22", GITHUB_WORKFLOW_REF: "Renstrio24p/OPENAI-TSGPT/.github/workflows/deploy.yml@refs/heads/main", PERFLOG_LOCATION_SETTING: "RUNNER_PERFLOG", GITHUB_ACTION_REPOSITORY: "", npm_config_node_gyp: "/opt/hostedtoolcache/node/16.20.2/x64/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js", PATH: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT/node_modules/.bin:/home/runner/work/OPENAI-TSGPT/node_modules/.bin:/home/runner/work/node_modules/.bin:/home/runner/node_modules/.bin:/home/node_modules/.bin:/node_modules/.bin:/opt/hostedtoolcache/node/16.20.2/x64/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/opt/hostedtoolcache/node/16.20.2/x64/bin:/snap/bin:/home/runner/.local/bin:/opt/pipx_bin:/home/runner/.cargo/bin:/home/runner/.config/composer/vendor/bin:/usr/local/.ghcup/bin:/home/runner/.dotnet/tools:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin", ANT_HOME: "/usr/share/ant", DOTNET_MULTILEVEL_LOOKUP: "0", RUNNER_TRACKING_ID: "github_7548975c-31c6-4b00-832d-b79c046580b5", INVOCATION_ID: "3eb2d85f9c4c4478865ce8a99256d7f1", RUNNER_TOOL_CACHE: "/opt/hostedtoolcache", GOROOT_1_19_X64: "/opt/hostedtoolcache/go/1.19.13/x64", NODE: "/opt/hostedtoolcache/node/16.20.2/x64/bin/node", npm_package_name: "ests", GITHUB_ACTION: "__run", GITHUB_RUN_NUMBER: "5", GITHUB_TRIGGERING_ACTOR: "Renstrio24p", RUNNER_ARCH: "X64", XDG_RUNTIME_DIR: "/run/user/1001", AGENT_TOOLSDIRECTORY: "/opt/hostedtoolcache", LANG: "C.UTF-8", VCPKG_INSTALLATION_ROOT: "/usr/local/share/vcpkg", CONDA: "/usr/share/miniconda", RUNNER_NAME: "GitHub Actions 3", XDG_CONFIG_HOME: "/home/runner/.config", STATS_VMD: "true", GITHUB_REF_NAME: "main", GITHUB_REPOSITORY: "Renstrio24p/OPENAI-TSGPT", npm_lifecycle_script: "rimraf ./dist && ts-node esbuild.config.ts -build", STATS_UE: "true", ANDROID_NDK_ROOT: "/usr/local/lib/android/sdk/ndk/25.2.9519653", GITHUB_ACTION_REF: "", DEBIAN_FRONTEND: "noninteractive", GITHUB_REPOSITORY_ID: "740085347", GITHUB_ACTIONS: "true", npm_package_version: "1.0.0", npm_lifecycle_event: "build", GITHUB_REF_PROTECTED: "false", GITHUB_WORKSPACE: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT", ACCEPT_EULA: "Y", GITHUB_JOB: "build", RUNNER_PERFLOG: "/home/runner/perflog", GITHUB_SHA: "9314875c810d8cd4714db15762111fa4ea8cefcd", GITHUB_RUN_ATTEMPT: "1", GITHUB_REF: "refs/heads/main", GITHUB_ACTOR: "Renstrio24p", ANDROID_SDK_ROOT: "/usr/local/lib/android/sdk", LEIN_HOME: "/usr/local/lib/lein", npm_config_globalconfig: "/opt/hostedtoolcache/node/16.20.2/x64/etc/npmrc", npm_config_init_module: "/home/runner/.npm-init.js", GITHUB_PATH: "/home/runner/work/_temp/_runner_file_commands/add_path_5ab42410-3d08-48c2-a5e1-b433bc7d1378", JAVA_HOME: "/usr/lib/jvm/temurin-11-jdk-amd64", PWD: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT", GITHUB_ACTOR_ID: "123795328", RUNNER_WORKSPACE: "/home/runner/work/OPENAI-TSGPT", npm_execpath: "/opt/hostedtoolcache/node/16.20.2/x64/lib/node_modules/npm/bin/npm-cli.js", HOMEBREW_CLEANUP_PERIODIC_FULL_DAYS: "3650", STATS_TIS: "mining", GITHUB_EVENT_NAME: "push", HOMEBREW_NO_AUTO_UPDATE: "1", ANDROID_HOME: "/usr/local/lib/android/sdk", GITHUB_SERVER_URL: "https://github.com", GECKOWEBDRIVER: "/usr/local/share/gecko_driver", LEIN_JAR: "/usr/local/lib/lein/self-installs/leiningen-2.10.0-standalone.jar", GHCUP_INSTALL_BASE_PREFIX: "/usr/local", GITHUB_OUTPUT: "/home/runner/work/_temp/_runner_file_commands/set_output_5ab42410-3d08-48c2-a5e1-b433bc7d1378", npm_config_global_prefix: "/opt/hostedtoolcache/node/16.20.2/x64", EDGEWEBDRIVER: "/usr/local/share/edge_driver", STATS_EXT: "true", npm_command: "run-script", ANDROID_NDK: "/usr/local/lib/android/sdk/ndk/25.2.9519653", SGX_AESM_ADDR: "1", CHROME_BIN: "/usr/bin/google-chrome", SELENIUM_JAR_PATH: "/usr/share/java/selenium-server.jar", STATS_EXTP: "https://provjobdsettingscdn.blob.core.windows.net/settings/provjobdsettings-0.5.154/provjobd.data", ANDROID_NDK_HOME: "/usr/local/lib/android/sdk/ndk/25.2.9519653", GITHUB_STEP_SUMMARY: "/home/runner/work/_temp/_runner_file_commands/step_summary_5ab42410-3d08-48c2-a5e1-b433bc7d1378", INIT_CWD: "/home/runner/work/OPENAI-TSGPT/OPENAI-TSGPT", EDITOR: "vi" };
+    }
+  });
+
+  // src/assets/css/app.css
+  var init_app = __esm({
+    "src/assets/css/app.css"() {
+    }
+  });
+
+  // src/pages/Chat/TSGPT.css
+  var init_TSGPT = __esm({
+    "src/pages/Chat/TSGPT.css"() {
+    }
+  });
+
+  // src/pages/Chat/TSGPT.ts
+  function TSGPT(DOM2) {
+    Render2();
+    function Render2() {
+      DOM2.innerHTML = `
+    <div class='body'>
+      <div id='dim' class='dim'></div>
+      <section class='sidebar' id='side'>
+        <div class='sbars' id='sbars'>
+          <i class='fa-solid fa-bars'></i>
+        </div>
+        <button>
+          <div class='new'>
+             <img src='Types.png' alt='ts' />
+          </div>
+            New chat
+            <i class="fa-solid fa-pen-to-square"></i>
+        </button>
+        <div class='history'>
+          <p>Hello World</p>
+          <p>What is 1 + 1</p>
+        </div>
+        <div class='nav'>
+          <p>Made by. Renstrio24p</p>
+        </div>
+      </section>
+      <section class='main'>
+        <div class='scroll'>
+          <div class='logo'>
+            <h1>TS GPT</h1>
+            <div class='icon'>
+              <img src='Types.png' alt='ts' />
+            </div>
+          </div>
+          </div>
+          <div class='bottom-section'>
+           <p id='output'>Test</p>
+           <div class='input-container'>
+              <input />
+              <div id='submit'>\u27A3</div>
+           </div>
+        </div>
+        <p class='info'>
+          Chat GPT Mar 14 vers. Free Research Preview.
+          Our goal is to make AI systems more natural and safe to interact with.
+          Your feedback will help us improve.
+        </p>
+      </section>
+    </div>
+  `;
+    }
+    const submitBtn = document.getElementById("submit");
+    const outputEl = document.getElementById("output");
+    const inputEl = document.querySelector("input");
+    const historyEl = document.querySelector(".history");
+    const buttonEl = document.querySelector("button");
+    const sbarBtn = document.getElementById("sbars");
+    const sidebarEL = document.getElementById("side");
+    const dimEl = document.getElementById("dim");
+    sbarBtn.addEventListener("click", () => {
+      sidebarEL.classList.toggle("active");
+      dimEl.classList.toggle("active");
+    });
+    function changeInput(value) {
+      const inputEl2 = document.querySelector("input");
+      inputEl2.value = value;
+    }
+    async function getMessage() {
+      console.log("clicked.");
+      const sanitizeInput = inputEl.value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+      const gptOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${define_process_env_default.OPENAI_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: sanitizeInput,
+              max_tokens: 100
+            }
+          ]
+        }
+      };
+      const requestOptions = {
+        method: gptOptions.method,
+        headers: gptOptions.headers,
+        body: JSON.stringify(gptOptions.body)
+      };
+      try {
+        const res = await fetch("https://api.openai.com/v1/chat/completions", requestOptions);
+        const data = await res.json();
+        console.log(data);
+        if (data.choices && data.choices[0] && data.choices[0].message && sanitizeInput) {
+          const pEl = document.createElement("p");
+          pEl.textContent = sanitizeInput;
+          pEl.addEventListener("click", () => changeInput(pEl.textContent = sanitizeInput));
+          historyEl?.append(pEl);
+        } else if (data.error) {
+          outputEl.textContent = data.error.message;
+          outputEl.classList.add("error");
+        } else {
+          outputEl.textContent = "Unexpected response format.";
+        }
+      } catch (error) {
+        console.error(`Error: ${error.message}`);
+      }
+    }
+    const clearInput = () => {
+      inputEl.value = "";
+    };
+    submitBtn?.addEventListener("click", getMessage);
+    buttonEl?.addEventListener("click", clearInput);
+  }
+  var init_TSGPT2 = __esm({
+    "src/pages/Chat/TSGPT.ts"() {
+      "use strict";
+      init_define_process_env();
+      init_TSGPT();
+    }
+  });
+
+  // src/assets/render/render.ts
+  function Render() {
+    const TSDOM = document.querySelector("#TS");
+    {
+      TSDOM && TSGPT(TSDOM);
+    }
+  }
+  var init_render = __esm({
+    "src/assets/render/render.ts"() {
+      "use strict";
+      init_define_process_env();
+      init_TSGPT2();
+    }
+  });
+
+  // src/start.ts
+  var start_exports = {};
+  __export(start_exports, {
+    default: () => Start
+  });
+  async function Start(start) {
+    start.innerHTML = `
+        <div>
+            <div id='TS'></div>
+        </div>
+    `;
+    Render();
+  }
+  var init_start = __esm({
+    "src/start.ts"() {
+      "use strict";
+      init_define_process_env();
+      init_app();
+      init_render();
+    }
+  });
+
+  // src/assets/security/hashes.ts
+  var hashes_exports = {};
+  __export(hashes_exports, {
+    default: () => UniqueHash
+  });
+  function UniqueHash() {
+    let hash;
+    do {
+      hash = Math.random().toString(36).substring(2);
+    } while (GenerateHashes.has(hash));
+    GenerateHashes.add(hash);
+    return hash;
+  }
+  var GenerateHashes;
+  var init_hashes = __esm({
+    "src/assets/security/hashes.ts"() {
+      "use strict";
+      init_define_process_env();
+      GenerateHashes = /* @__PURE__ */ new Set();
+    }
+  });
+
+  // src/index.ts
+  init_define_process_env();
+  var loadStart = async () => {
+    const module = await Promise.resolve().then(() => (init_start(), start_exports));
+    return module.default;
+  };
+  var loadUniqueHash = async () => {
+    const module = await Promise.resolve().then(() => (init_hashes(), hashes_exports));
+    return module.default;
+  };
+  var DOM = document.querySelector("#app");
+  Promise.all([loadStart(), loadUniqueHash()]).then(([Start2, UniqueHash2]) => {
+    DOM.id = UniqueHash2();
+    Start2(DOM);
+  });
+})();
